f8e0c90e2bc44e4a606edc99eea042c1b3a380fb
Jeremy Hinegardner <jeremy@hinegardner.org>
2009-05-11 23:10:28 -0600

    savepoint

--- lib/crate/task/unpack.rb
+++ lib/crate/task/unpack.rb
@@ -1,11 +1,31 @@
+require 'rubygems'
+require 'gem/installer'
 module Crate
   class Task
     class Unpack < ::Crate::Task
+
       # The File to unpack
       config 'file'
 
       def initialize( *args )
         super
+      end
+
+      #
+      # Changes into a directory and unpacks the archive.
+      #
+      def unpack( archive, into = Dir.pwd )
+        Dir.chdir( into ) do 
+          if archive.match( /\.tar\.gz\Z/ ) or archive.match(/\.tgz\Z/) then
+            tgz = ::Zlib::GzipReader.new( File.open( local_source, 'rb') )
+            ::Archive::Tar::Minitar.unpack( tgz, into )
+          elsif archive.match( /\.gem\Z/ ) then
+            subdir = File.basename( archive, ".gem" )
+            Gem::Installer.new( archive ).unpack( subdir )
+          else
+            raise "Unable to extract files from #{File.basename( local_source)} -- unknown format"
+          end
+        end
       end
 
       def process( *args )
